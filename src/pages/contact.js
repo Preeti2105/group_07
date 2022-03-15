ff3dbaaa0040a0bce9884ec6f064eb14d168f8bb
Preeti2105 <ps15068@gmail.com>
2022-03-15 12:11:16 +0530

    updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
 
-export default function Home() {
-  return <div>Contact</div>
+const Contact = () => {
+  return (
+    <Layout>
+      <div className="container py-5">
+        <h2 className="pb-2 border-bottom text-center">Contact</h2>{" "}
+      </div>
+    </Layout>
+  )
 }
+
+export default Contact
+
+
