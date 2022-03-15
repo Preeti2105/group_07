0181387cc892fdb323a1287e973bccff26a184bc
Preeti2105 <ps15068@gmail.com>
2022-03-15 11:37:58 +0530

    updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
 
-const About = ()=> 
-{
-  (
-  <Layout>      
+const About = () => {
   return (
-  <h1 classNAme="text-success">About</h1>
-  </Layout>
+    <Layout>
+      <div className="container py-5">
+        <h2 className="pb-2 border-bottom text-center">About</h2>
+      </div>
+    </Layout>
   )
 }
+
 export default About
+
+
+
